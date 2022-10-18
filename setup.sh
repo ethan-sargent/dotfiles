1737e6b3e3653f432ade55cf3e77c31112a740e5
ethansargent <ethan.sargent@icloud.com>
2022-10-18 21:32:28 +1100

    updated and tested setup script

--- setup.sh
+++ setup.sh
@@ -2,4 +2,16 @@
 #
 # Usage: Run when first pulling dotfiles to ensure configuration files are linked to the repo
 #
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+printf $SCRIPT_DIR
+# ensure XDG_CONFIG_HOME is set
+source $SCRIPT_DIR/zsh/.zshenv
+
+# symlink zsh and nvim
+ln -s "$SCRIPT_DIR"/zsh "$XDG_CONFIG_HOME"/zsh
+ln -s "$SCRIPT_DIR"/nvim "$XDG_CONFIG_HOME"/nvim
+
+# TODO: make tmux XDG compliant and symlink
+
+# TODO: Copy alacritty config based on OS due to Mac config incompatiblities
 
