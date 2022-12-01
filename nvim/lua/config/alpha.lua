7e0c0d1a81942ef76b02870587cef3cfb569ee21
Ethan Sargent <ethan.sargent@accenture.com>
2022-12-01 20:50:27 +1100

    added new startup config

---
+++ nvim/lua/config/alpha.lua
@@ -0,0 +1,33 @@
+local dashboard = require'alpha.themes.dashboard';
+
+local hydra = {
+  [[⣴⣶⣤⡤⠦⣤⣀⣤⠆     ⣈⣭⣭⣿⣶⣿⣦⣼⣆          ]],
+  [[   ⠉⠻⢿⣿⠿⣿⣿⣶⣦⠤ ⡠⢾⣿⣿⡿⠋⠉⠉⠻⣿⣿⡛⣦      ]],
+  [[     ⠈   ⠈⢿⣿⣟⠦ ⣾⣿⣿⣷    ⠻⠿⢿⣿⣧⣄    ]],
+  [[          ⣸⣿⣿⢧ ⢻⠻⣿⣿⣷⣄⣀ ⠢⣀⡀⠈⠙⠿    ]],
+  [[  ⢀      ⢠⣿⣿⣿⠈  ⠡⠌⣻⣿⣿⣿⣿⣿⣿⣿⣛⣳⣤⣀⣀  ]],
+  [[  ⢠⣧⣶⣥⡤⢄ ⣸⣿⣿⠘  ⢀⣴⣿⣿⡿⠛⣿⣿⣧⠈⢿⠿⠟⠛⠻⠿  ]],
+  [[ ⣰⣿⣿⠛⠻⣿⣿⡦⢹⣿⣷   ⢊⣿⣿⡏  ⢸⣿⣿⡇ ⢀⣠⣄⣾   ]],
+  [[⣠⣿⠿⠛ ⢀⣿⣿⣷⠘⢿⣿⣦⡀ ⢸⢿⣿⣿⣄ ⣸⣿⣿⡇⣪⣿⡿⠿⣿⣷⡄ ]],
+  [[⠙⠃   ⣼⣿⡟⠌ ⠈⠻⣿⣿⣦⣌⡇⠻⣿⣿⣷⣿⣿⣿⠐⣿⣿⡇ ⠛⠻⢷⣄]],
+  [[     ⢻⣿⣿⣄   ⠈⠻⣿⣿⣿⣷⣿⣿⣿⣿⣿⡟ ⠫⢿⣿⡆   ⠁]],
+  [[      ⠻⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⢀⣀⣤⣾⡿⠃    ]],
+};
+
+dashboard.section.header.val = hydra
+
+dashboard.section.buttons.val = {
+    dashboard.button( "n", "  New file" , ":ene <BAR> startinsert <CR>"),
+    dashboard.button( "f", "  Find file", ":Telescope find_files<CR>"),
+    dashboard.button( "r", "  Recent"   , ":Telescope oldfiles<CR>"),
+    dashboard.button( "s", "  Settings" , ":e $MYVIMRC | :cd %:p:h | pwd<CR>"),
+    dashboard.button( "q", "  Quit NVIM", ":qa<CR>"),
+}
+
+require("alpha").setup(dashboard.config)
+-- vim.cmd([[
+--     augroup alpha_config
+--       au!
+--       autocmd FileType alpha setlocal nofoldenable
+--     augroup
+-- ]])
