b4ca78ed19a2d4b9df81e5f710a4986b97d67a62
Ethan Sargent <ethan.sargent@accenture.com>
2022-12-02 21:33:32 +1100

    SFDX Autodeployment Automatic SFDX Check and Deploy scripts based on Git revision history

--- zsh/sfdx.zsh
+++ zsh/sfdx.zsh
@@ -15,8 +15,21 @@ dxalias() {
   echo $(jq .defaultusername -r .sfdx/sfdx-config.json)
 }
 
+local dx_saveincref() {
+  local head_commit=$(git rev-parse HEAD) 
+  echo $head_commit > .sfdx/.lastincref
+}
+
+local dxautoinc() {
+  local head_commit=$(git rev-parse HEAD) 
+  echo $head_commit > .sfdx/.lastincref
+  local branch_forkpoint=$(git merge-base --fork-point HEAD acfr-qfr/rolling)
+
+  sfdx sfpowerkit:project:diff -d "$incdir" -r "$1" -x
+}
+
 # commit ID
-dxinc() {
+dxincbuild() {
   local incdir="${2:-deploy}"
   test -d "$incdir" && rm -r "$incdir" && mkdir -p "$incdir"
   sfdx sfpowerkit:project:diff -d "$incdir" -r "$1" -x
@@ -25,17 +38,36 @@ dxinc() {
 # commit id, org to validate against, dir to package into (optional)
 # dxinccheck a0d1ebe72
 dxinccheck(){
+  local fork_point=$(git merge-base acfr-qfr/rolling HEAD --fork-point)
+  local commit_id="${1:-$fork_point}"
   local incdir="${3:-deploy}"
   local user="${2:-dit}"
   test -d "$incdir" && rm -r "$incdir" && mkdir -p "$incdir"
-  sfdx sfpowerkit:project:diff -d "$incdir" -r "$1" -x 
+  sfdx sfpowerkit:project:diff -d "$incdir" -r "$commit_id" -x 
   sfdx force:source:deploy --checkonly -p "$incdir"/force-app -u "$user"
 }
 
-dxincdeploy(){
+dxincautodeploy() {
+  trap 'tput cnorm; exit' INT TERM
+  if [[ ! -f .sfdx/.lastincref ]]; then
+    >&2 echo "No previous commit ref stored in .sfdx/.lastincref"
+  fi
+  last_commit="${1:-$(< .sfdx/.lastincref)}"
+  if [[ -z $last_commit ]]; then
+    echo "No commit id provided. Exiting..."
+    return 1;
+  fi
+  local head_commit=$(git rev-parse HEAD) 
+  if [[ $head_commit = $last_commit ]]; then
+    echo "current HEAD matches previous autodeploy ref. Exiting..."
+    return 1;
+  fi
+  echo "Previous commit hash $last_commit - starting delta build..."
   local incdir="${2:-deploy}"
   local user="${1:-dit}"
+  sfdx sfpowerkit:project:diff -d "$incdir" -r "$last_commit" -x 
   sfdx force:source:deploy -p "$incdir"/force-app -u "$user"
+  echo $head_commit > .sfdx/.lastincref
+  echo "$head_commit saved as last incremental commit ID"
 }
-
 
