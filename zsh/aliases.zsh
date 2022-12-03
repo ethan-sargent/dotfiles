bd7aa0f9d55aad3be46e192a5522ab22b11645e4
ethansargent <ethan.sargent@icloud.com>
2022-12-03 13:32:54 +1100

    added v as nvim alias

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,7 +1,10 @@
 # alias commands if they exist
 # preferred format for speed with zsh
 
-(($+commands[nvim]))  && { alias vim="nvim" } 
+(($+commands[nvim]))  && { 
+  alias vim="nvim"
+  alias v="nvim"
+} 
 (($+commands[exa]))   && { alias ls="exa"   } 
 (($+commands[bat]))   && { alias cat="bat"  } 
 (($+commands[just]))  && { alias j="just"   } 
