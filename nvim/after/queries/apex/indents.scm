aee7d233d0670bbcafc8cbc58d38c7ce43798fad
Ethan Sargent <ethan.sargent@icloud.com>
2023-04-11 20:07:08 +1000

    nvim: update apex indents queries

--- nvim/after/queries/apex/indents.scm
+++ nvim/after/queries/apex/indents.scm
@@ -2,44 +2,34 @@
  (class_body)
  (enum_body)
  (interface_body)
- (constructor_declaration)
  (constructor_body)
  (block)
  (switch_block)
- (field_declaration)
  (argument_list)
- (local_variable_declaration)
- (annotation_argument_list)
- (assignment_expression)
- (parenthesized_expression)
  (formal_parameters)
- (ternary_expression)
  (query_expression)
- ; (soql_query)
  (subquery)
- (accessor_list)
-] @indent
+] @indent.begin
 
 
-(soql_query_body 
-  (_) @indent)
+(soql_query_body (_) @indent.begin)
 
 [
  ")"
  "}"
  "]"
-] @branch
+] @indent.branch
 
 [
  "}"
- "]"
- ")"
-] @indent_end
+ ; "]"
+ ; ")"
+] @indent.end
 
-(line_comment) @ignore
+(line_comment) @indent.ignore
 
 [
  (ERROR)
  (block_comment)
-] @auto
+] @indent.auto
 
